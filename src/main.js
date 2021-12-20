00c9c97eb7c56c54a6635a9fbb5774d9e9f47e3a
DoThiHongThao <hongthao2491999@gmail.com>
2021-12-20 23:13:59 +0700

    [1] add ant

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import Button from 'ant-design-vue/lib/button';
+import 'ant-design-vue/dist/antd.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.component(Button.name, Button);
+
+app.config.productionTip = false;
+app.mount('#app');
